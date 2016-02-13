5db65099b2b6df03b4b128febb270718dda56825
Ryan Moore <ryan.moore@greyrocksoft.com>
2016-02-12 20:29:06 -0700

    Inital code commit

---
+++ dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/Main.java
@@ -0,0 +1,15 @@
+package com.spectralogic.dsbrowser.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(final String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+
+    }
+}
