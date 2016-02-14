fe0cbe124046341c64837e8a326437ea3724128b
Ryan Moore <ryan.moore@greyrocksoft.com>
2016-02-13 18:07:39 -0700

    Enabled logging from within the Injector

--- dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/Main.java
+++ dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/Main.java
@@ -1,22 +1,24 @@
 package com.spectralogic.dsbrowser.gui;
 
 import com.airhacks.afterburner.injection.Injector;
-import com.google.common.collect.ImmutableMap;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main extends Application {
+
     public static void main(final String[] args) {
         launch(args);
     }
 
     @Override
     public void start(final Stage primaryStage) throws Exception {
+        final Logger injectorLogger = LoggerFactory.getLogger("Injector");
         primaryStage.setTitle("Deep Storage Browser v0.0.1");
 
+        Injector.setLogger(injectorLogger::info);
         final DeepStorageBrowserView mainView = new DeepStorageBrowserView();
 
         final Scene mainScene = new Scene(mainView.getView());
