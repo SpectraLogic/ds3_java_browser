167676de769404cbfdccb6aaa7f35133fa02001f
Sharon Shabtai <sharons@spectralogic.com>
2017-01-30 14:14:15 -0700

    create a new exe to run the gui

--- Builders/Windows/SetupProject/DsbExecutable/Properties/AssemblyInfo.cs
+++ Builders/Windows/SetupProject/DsbExecutable/Properties/AssemblyInfo.cs
@@ -1,35 +1,36 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DeleteLogsCustomAction")]
+[assembly: AssemblyTitle("dsb-gui")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Spectra Logic Corporation")]
-[assembly: AssemblyProduct("DeleteLogsCustomAction")]
+[assembly: AssemblyProduct("dsb-gui")]
 [assembly: AssemblyCopyright("Copyright © Spectra Logic Corporation 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("283bf1c6-2f2b-4bb1-9e86-039df2cfb241")]
+[assembly: Guid("5c0b65a4-18fb-485c-a35b-203d5faa97d6")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.40.0")]
+[assembly: AssemblyFileVersion("2.0.40.0")]
