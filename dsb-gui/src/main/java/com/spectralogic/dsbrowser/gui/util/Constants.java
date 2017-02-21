fc8f0af88ede3bf47d71c2f5fffef638b69615cf
Suman Chahar <suman.chahar@vvdntech.in>
2017-02-21 19:19:32 +0530

    code changes for the issues identified during internal testing.

--- dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/util/Constants.java
+++ dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/util/Constants.java
@@ -1,9 +1,9 @@
 package com.spectralogic.dsbrowser.gui.util;
 
 public interface Constants {
-    int OPEN_DELAY = 200;
+    int OPEN_DELAY = 100;
     int CLOSE_DELAY = 0;
-    int DURATION = 1000;
+    int DURATION = 2000;
     int RETRY_AFTER_COUNT = 100;
     int MAX_PARALLEL_THREAD_DEFAULT = 10;
     String PORT_NUMBER = "80";
