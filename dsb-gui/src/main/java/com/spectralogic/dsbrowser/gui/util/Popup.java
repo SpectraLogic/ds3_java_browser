77fa0b06cebffd50aa3f33982c4ba9a8e3cf8031
DenverM80 <denverm@spectralogic.com>
2017-09-28 13:28:31 -0600

    Fix another reference to DSB_ICON_PATH; Enlarge the About panel to allow for OS / preferred text size differences

--- dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/util/Popup.java
+++ dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/util/Popup.java
@@ -21,16 +21,14 @@ import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.util.ResourceBundle;
 
 public final class Popup {
     public static void show(final Parent parent, final String title) {
-        final ResourceBundle resourceBundle = ResourceBundleProperties.getResourceBundle();
         final Stage popup = new Stage();
         popup.initModality(Modality.APPLICATION_MODAL);
         popup.setMaxWidth(1000);
         final Scene popupScene = new Scene(parent);
-        popup.getIcons().add(new Image(resourceBundle.getString("dsbIconPath")));
+        popup.getIcons().add(new Image(StringConstants.DSB_ICON_PATH));
         popup.setScene(popupScene);
         popup.setTitle(title);
         popup.setAlwaysOnTop(false);
