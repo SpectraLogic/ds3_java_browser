980833139a4ea58c5d0c8378956a085ecdd32892
scribe <bandwidthoracle@gmail.com>
2017-10-03 15:14:34 -0600

    Working on logic

--- dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/components/ds3panel/ds3treetable/Ds3TreeTablePresenter.java
+++ dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/components/ds3panel/ds3treetable/Ds3TreeTablePresenter.java
@@ -426,7 +426,7 @@ public class Ds3TreeTablePresenter implements Initializable {
 
                 }));
                 putJob.setOnFailed(SafeHandler.logHandle(e -> {
-                    LOG.info("setOnFailed");
+                    LOG.error("PutJob Failed", e);
                     Ds3PanelService.refresh(selectedItem);
                     ds3TreeTable.getSelectionModel().clearSelection();
                     ds3TreeTable.getSelectionModel().select(selectedItem);
