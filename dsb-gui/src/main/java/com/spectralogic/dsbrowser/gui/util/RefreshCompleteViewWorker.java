629449ee914ec96f701bd0e55068f554e902c64d
scribe <bandwidthoracle@gmail.com>
2017-10-12 14:59:32 -0600

    No longer causes strange behavior

--- dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/util/RefreshCompleteViewWorker.java
+++ dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/util/RefreshCompleteViewWorker.java
@@ -18,6 +18,7 @@ package com.spectralogic.dsbrowser.gui.util;
 import com.spectralogic.dsbrowser.api.services.logging.LogType;
 import com.spectralogic.dsbrowser.api.services.logging.LoggingService;
 import com.spectralogic.dsbrowser.gui.components.ds3panel.Ds3Common;
+import com.spectralogic.dsbrowser.gui.components.ds3panel.Ds3PanelPresenter;
 import com.spectralogic.dsbrowser.gui.components.ds3panel.ds3treetable.Ds3TreeTableItem;
 import com.spectralogic.dsbrowser.gui.components.ds3panel.ds3treetable.Ds3TreeTableValue;
 import com.spectralogic.dsbrowser.gui.services.Workers;
@@ -25,11 +26,9 @@ import com.spectralogic.dsbrowser.gui.services.sessionStore.Session;
 import com.spectralogic.dsbrowser.gui.services.tasks.GetServiceTask;
 import com.spectralogic.dsbrowser.gui.util.treeItem.SafeHandler;
 import javafx.beans.property.BooleanProperty;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableColumn;
-import javafx.scene.control.TreeTableView;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,53 +39,57 @@ public final class RefreshCompleteViewWorker {
     private final static Logger LOG = LoggerFactory.getLogger(RefreshCompleteViewWorker.class);
 
     public static void refreshCompleteTreeTableView(final Ds3Common ds3Common, final Workers workers, final DateTimeUtils dateTimeUtils, final LoggingService loggingService) {
-        if (ds3Common.getCurrentSession() != null && ds3Common.getCurrentTabPane() != null) {
-            final Session session = ds3Common.getCurrentSession();
+        final Session session = ds3Common.getCurrentSession();
+        if (session != null && ds3Common.getCurrentTabPane() != null) {
             loggingService.logMessage("Refreshing session " + session.getSessionName() +
                     StringConstants.SESSION_SEPARATOR +
                     session.getEndpoint(), LogType.INFO);
             @SuppressWarnings("unchecked")
             final TreeTableView<Ds3TreeTableValue> ds3TreeTableView = getTreeTableView(ds3Common);
-            ds3Common.getDs3PanelPresenter().getDs3PathIndicator().setText(null);
-            ds3Common.getDs3PanelPresenter().getDs3PathIndicator().setTooltip(null);
+            final Ds3PanelPresenter ds3PanelPresenter = ds3Common.getDs3PanelPresenter();
+            final Label ds3PathIndicator = ds3PanelPresenter.getDs3PathIndicator();
+            ds3PathIndicator.setText(null);
+            ds3PathIndicator.setTooltip(null);
             if (ds3TreeTableView != null && ds3TreeTableView.getColumns() != null) {
                 final TreeItem<Ds3TreeTableValue> selectedRoot = ds3TreeTableView.getRoot();
+                final TreeTableView.TreeTableViewSelectionModel<Ds3TreeTableValue> selectionModel = ds3TreeTableView.getSelectionModel();
                 if (selectedRoot != null && selectedRoot.getValue() != null && selectedRoot.getParent() != null) {
-                    ds3TreeTableView.getSelectionModel().clearSelection();
+                    selectionModel.clearSelection();
                     ds3TreeTableView.setRoot(selectedRoot);
-                    ds3TreeTableView.getSelectionModel().select(selectedRoot);
+                    selectionModel.select(selectedRoot);
                     ((Ds3TreeTableItem) selectedRoot).refresh();
                     setPathIndicator((Ds3TreeTableItem)selectedRoot , ds3Common);
-                    ds3Common.getDs3PanelPresenter().calculateFiles(ds3TreeTableView);
+                    ds3PanelPresenter.calculateFiles(ds3TreeTableView);
                 } else {
                     if(selectedRoot != null && selectedRoot.getParent() == null) {
                         LOG.warn("Parent folder no longer existed, redirecting to the root of the tree");
                     }
                     final TreeItem<Ds3TreeTableValue> rootTreeItem = new TreeItem<>();
-                    final GetServiceTask getServiceTask = new GetServiceTask(rootTreeItem.getChildren(), session, workers, ds3Common, dateTimeUtils, loggingService);
+                    final ObservableList<TreeItem<Ds3TreeTableValue>> children = rootTreeItem.getChildren();
+                    final GetServiceTask getServiceTask = new GetServiceTask(children, session, workers, ds3Common, dateTimeUtils, loggingService);
                     getServiceTask.setOnSucceeded(SafeHandler.logHandle(event -> {
                         ds3TreeTableView.setRoot(rootTreeItem);
                         if (ds3Common.getExpandedNodesInfo().containsKey(session.getSessionName() + StringConstants.SESSION_SEPARATOR +
                                 session.getEndpoint())) {
-                            rootTreeItem.getChildren().forEach(i ->
+                            children.forEach(i ->
                                     i.expandedProperty().addListener((observable, oldValue, newValue) -> {
                                         final BooleanProperty bb = (BooleanProperty) observable;
                                         final TreeItem<Ds3TreeTableValue> bean = (TreeItem<Ds3TreeTableValue>) bb.getBean();
                                         ds3Common.getExpandedNodesInfo().put(session.getSessionName() + "-" + session.getEndpoint(), bean);
                                     }));
                             final TreeItem<Ds3TreeTableValue> item = ds3Common.getExpandedNodesInfo().get(session.getSessionName() + StringConstants.SESSION_SEPARATOR + session.getEndpoint());
-                            if (rootTreeItem.getChildren().stream().anyMatch(i -> i.getValue().getBucketName().equals(item.getValue().getBucketName()))) {
-                                final TreeItem<Ds3TreeTableValue> ds3TreeTableValueTreeItem = rootTreeItem.getChildren().stream().filter(i -> i.getValue().getBucketName().equals(item.getValue().getBucketName())).findFirst().get();
+                            if (children.stream().anyMatch(i -> i.getValue().getBucketName().equals(item.getValue().getBucketName()))) {
+                                final TreeItem<Ds3TreeTableValue> ds3TreeTableValueTreeItem = children.stream().filter(i -> i.getValue().getBucketName().equals(item.getValue().getBucketName())).findFirst().get();
                                 ds3TreeTableValueTreeItem.setExpanded(false);
                                 if (!ds3TreeTableValueTreeItem.isLeaf() && !ds3TreeTableValueTreeItem.isExpanded()) {
-                                    ds3TreeTableView.getSelectionModel().select(ds3TreeTableValueTreeItem);
+                                    selectionModel.select(ds3TreeTableValueTreeItem);
                                     ds3TreeTableValueTreeItem.setExpanded(true);
                                     setPathIndicator((Ds3TreeTableItem)ds3TreeTableValueTreeItem , ds3Common);
                                 }
                             }
                         } else {
-                            ds3Common.getDs3PanelPresenter().getDs3PathIndicator().setText(StringConstants.EMPTY_STRING);
-                            ds3Common.getDs3PanelPresenter().getDs3PathIndicator().setTooltip(null);
+                            ds3PathIndicator.setText(StringConstants.EMPTY_STRING);
+                            ds3PathIndicator.setTooltip(null);
                         }
                     }));
                     getServiceTask.setOnFailed(SafeHandler.logHandle(event -> {
