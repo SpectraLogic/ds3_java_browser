b60450beff3eb4c00a4c7d9d256bab0e0e9ef722
scribe <bandwidthoracle@gmail.com>
2017-10-13 14:43:13 -0600

    Removed unused constants

--- dsb-gui/src/test/java/com/spectralogic/dsbrowser/gui/util/SessionConstants.java
+++ dsb-gui/src/test/java/com/spectralogic/dsbrowser/gui/util/SessionConstants.java
@@ -16,16 +16,8 @@
 package com.spectralogic.dsbrowser.gui.util;
 
 public interface SessionConstants {
-    String DS3_PANEL_SERVICE_TEST_BUCKET_NAME = "EMPTY_BUCKET";
-    String DS3_PANEL_SERVICE_TEST_FOLDER_NAME = "EMPTY_FOLDER";
-    String CREATE_BUCKET_TASK_TEST_BUCKET_NAME = "TEMPORARY_BUCKET";
     String ALREADY_EXIST_BUCKET = "TEST1";
-    String ALREADY_EXIST_FOLDER = "Files";
-    String ALREADY_EXIST_FILES = "SampleFiles.txt";
     String FOLDER_INSIDE_EXISTING_BUCKET = "temp";
     String LOCAL_FOLDER = "files/";
     String LOCAL_FILE = "SampleFiles.txt";
-    String DATA_POLICY_NAME = "fake";
-    String DATA_POLICY_UUID = "b8ae2e65-b665-4733-bd48-f7ab760c43f3";
-    String TEXT_TO_SEARCH = "11";
 }
