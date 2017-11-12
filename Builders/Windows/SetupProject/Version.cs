72b4e76c26d8c0c38b70e26ab35fce657f317029
Sharon Shabtai <sharons@spectralogic.com>
2017-11-12 06:24:51 -0700

    update icon

--- Builders/Windows/SetupProject/Version.cs
+++ Builders/Windows/SetupProject/Version.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.1.3.115")]
-[assembly: AssemblyFileVersion("2.1.3.115")]
+[assembly: AssemblyVersion("2.1.3.116")]
+[assembly: AssemblyFileVersion("2.1.3.116")]
