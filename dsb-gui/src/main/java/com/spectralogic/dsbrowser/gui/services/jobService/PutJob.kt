f0abce24526d0f086080a900f655767b672949e9
scribe <bandwidthoracle@gmail.com>
2018-01-11 11:59:41 -0700

    Typo

--- dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/services/jobService/PutJob.kt
+++ dsb-gui/src/main/java/com/spectralogic/dsbrowser/gui/services/jobService/PutJob.kt
@@ -33,12 +33,11 @@ import org.slf4j.LoggerFactory
 import java.util.*
 import java.util.concurrent.TimeUnit
 import java.util.function.Supplier
-import kotlin.reflect.KFunction0
 
 class PutJob(private val putJobData: JobData) : JobService(), PrepStage<JobData>, TransferStage, TeardownStage {
     override fun getDs3Client(): Ds3Client = putJobData.client()
 
-    private val chunkManagment: ChunkManagment = ChunkManagment()
+    private val chunkManagement: ChunkManagment = ChunkManagment()
     private val stats: Stats = Stats(message, putJobData.loggingService(), putJobData.dateTimeUtils())
 
     private companion object {
@@ -77,7 +76,7 @@ class PutJob(private val putJobData: JobData) : JobService(), PrepStage<JobData>
             sent.set(it + sent.get())
             stats.updateStatistics(putJobData.lastFile, putJobData.getStartTime(), sent, totalJob, totalJobMessage, putJobData.targetPath(), putJobData.bucket, false)
         }
-        job.attachWaitingForChunksListener { chunkManagment.waitForChunks(it, putJobData.loggingService(), LOG) }
+        job.attachWaitingForChunksListener { chunkManagement.waitForChunks(it, putJobData.loggingService(), LOG) }
         job.attachObjectCompletedListener {
             putJobData.lastFile = it
             stats.updateStatistics(putJobData.lastFile, putJobData.getStartTime(), sent, totalJob, totalJobMessage, putJobData.targetPath(), putJobData.bucket, true)
