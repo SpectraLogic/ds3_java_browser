909916cd8425f4f798216a6d072aa1655254fa56
Sharon Shabtai <sharons@spectralogic.com>
2018-01-15 09:57:41 -0700

    update windows setup project for 2.1.5 release

--- Builders/Windows/SetupProject/Version.cs
+++ Builders/Windows/SetupProject/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.1.4.0")]
-[assembly: AssemblyFileVersion("2.1.4.0")]
+[assembly: AssemblyVersion("2.1.5.3")]
+[assembly: AssemblyFileVersion("2.1.5.3")]
