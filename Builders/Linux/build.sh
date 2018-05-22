1aeb256b1622a8fcf9790cac6216dbb5805e3866
scribe <bandwidthoracle@gmail.com>
2018-05-22 10:50:43 -0600

    Version bump

--- Builders/Linux/build.sh
+++ Builders/Linux/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION='5.0.0'
+VERSION='5.0.1'
 
 javapackager -deploy -native deb -srcfiles ../../dsb-gui/build/libs/dsb-gui-$VERSION-all.jar -outdir deploy -appclass com.spectralogic.dsbrowser.gui.Main -outfile BlackPearlEonBrowser -name BlackPearlEonBrowser -Bicon=icon_128x128.png -BappVersion=$VERSION -v
