5c3ebec79369ebbd5443f5081fbb489255e965fb
scribe <bandwidthoracle@gmail.com>
2018-06-19 14:28:36 -0600

    Version bump 5.0.2

--- Builders/Mac/build.sh
+++ Builders/Mac/build.sh
@@ -2,7 +2,7 @@
 
 CODESIGN_IDENTITY='Developer ID Application: Spectra Logic Corporation (YAUF7295LE)'
 
-VERSION='5.0.0'
+VERSION='5.0.2'
 
 #/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home/bin/javapackager -deploy -native dmg -srcfiles ../../dsb-gui/build/libs/dsb-gui-$VERSION-all.jar -outdir deploy -appclass com.spectralogic.dsbrowser.gui.Main -outfile BlackPearlEonBrowser -name BlackPearlEonBrowser -Bmac.signing-key-developer-id-app="$CODESIGN_IDENTITY" -BappVersion=$VERSION -Bicon=eonbrowse.icns -v
 /Library/Java/JavaVirtualMachines/jdk1.8.0_162.jdk/Contents/Home/bin/javapackager -deploy -native dmg -srcfiles ../../dsb-gui/build/libs/dsb-gui-$VERSION-all.jar -outdir deploy -appclass com.spectralogic.dsbrowser.gui.Main -outfile BlackPearlEonBrowser -name BlackPearlEonBrowser -BappVersion=$VERSION -Bicon=eonbrowse.icns -v
