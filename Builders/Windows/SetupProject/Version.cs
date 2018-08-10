b80c3a62a98cc488238316e23ba08b291e834cae
Sharon Shabtai <sharons@spectralogic.com>
2018-08-10 15:15:25 -0600

    Bump the version

--- Builders/Windows/SetupProject/Version.cs
+++ Builders/Windows/SetupProject/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("5.0.3.4")]
-[assembly: AssemblyFileVersion("5.0.3.4")]
+[assembly: AssemblyVersion("5.0.4.1")]
+[assembly: AssemblyFileVersion("5.0.4.1")]
