b47c559ec7c2c517d89cdb3fa686ef5395c881c3
Sharon Shabtai <sharons@spectralogic.com>
2018-10-08 11:00:58 -0600

    Bump the version and add productionBuild=true env var

--- Builders/Windows/SetupProject/Version.cs
+++ Builders/Windows/SetupProject/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("5.0.6.1")]
-[assembly: AssemblyFileVersion("5.0.6.1")]
+[assembly: AssemblyVersion("5.0.7.2")]
+[assembly: AssemblyFileVersion("5.0.7.2")]
