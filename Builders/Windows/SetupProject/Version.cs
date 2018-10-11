a3d0659e3a36a5e9415db76e10c2c15b714a8c69
Sharon Shabtai <sharons@spectralogic.com>
2018-10-11 00:09:45 -0600

    Update the windows builder to v5.0.7

--- Builders/Windows/SetupProject/Version.cs
+++ Builders/Windows/SetupProject/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("5.0.6.2")]
-[assembly: AssemblyFileVersion("5.0.6.2")]
+[assembly: AssemblyVersion("5.0.7.1")]
+[assembly: AssemblyFileVersion("5.0.7.1")]
