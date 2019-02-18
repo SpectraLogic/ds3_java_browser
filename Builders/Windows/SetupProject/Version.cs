b7a12aa85c20bc0a0cde0f41ab847ecd1d5d9147
Sharon Shabtai <sharons@spectralogic.com>
2019-02-18 14:18:31 -0700

    Prepare for v_5.0.8

--- Builders/Windows/SetupProject/Version.cs
+++ Builders/Windows/SetupProject/Version.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("5.0.8.1")]
-[assembly: AssemblyFileVersion("5.0.8.1")]
+[assembly: AssemblyVersion("5.0.8.2")]
+[assembly: AssemblyFileVersion("5.0.8.2")]
