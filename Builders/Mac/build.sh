ba4df3e70e63091ef05a97ff1d414bb55d27d322
Eric <192110+scribe@users.noreply.github.com>
2020-04-03 11:14:39 -0600

    Moving over code

--- Builders/Mac/build.sh
+++ Builders/Mac/build.sh
@@ -2,7 +2,7 @@
 
 CODESIGN_IDENTITY='Developer ID Application: Spectra Logic Corporation (YAUF7295LE)'
 
-VERSION='5.0.10'
+VERSION='5.0.11'
 
 /Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home/bin/javafxpackager -deploy -native dmg -srcfiles ../../dsb-gui/build/libs/dsb-gui-$VERSION-all.jar -outdir deploy -appclass com.spectralogic.dsbrowser.gui.Main -outfile BlackPearlEonBrowser -name BlackPearlEonBrowser -BappVersion=$VERSION -Bicon=eonbrowse.icns -v
 
